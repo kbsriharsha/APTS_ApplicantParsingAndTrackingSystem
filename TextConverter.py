4b8f03d17d567a46923da5cca39104fe4ffc3f6b
Bharat Sri Harsha Karpurapu <kbsriharsha@Bharats-MacBook-Pro.local>
2019-11-18 13:36:49 -0500

    testing

---
+++ TextConverter.py
@@ -0,0 +1,75 @@
+# !/Users/kbsriharsha/anaconda3/bin/python
+# coding: utf-8
+# @author: Bharat Sri Harsha karpurapu
+
+"""
+This program helps converting different formats of the
+input file (pdf, doc, etc) to text (.txt)
+"""
+
+# Importing libraries
+import sys
+import os
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
+#from cStringIO import StringIO #use this for python2
+'''
+additional libraries for other formats
+
+#from pyth.plugins.rtf15.reader import Rtf15Reader
+#from pyth.plugins.plaintext.writer import PlaintextWriter
+#from docx import Document
+'''
+
+def convertPDFToText(path):
+    """
+    This function converts pdf to text
+
+    path: path of the file to be converted
+    """
+    fp = open(path, 'rb')
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, laparams=laparams)
+    # Create a PDF interpreter object.
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    # Process each page contained in the document.
+
+    for page in PDFPage.get_pages(fp):
+        interpreter.process_page(page)
+        data =  retstr.getvalue()
+
+    return data
+
+'''
+def convertRtfToText(path):
+    """
+    This function converts Rtf to text
+
+    path: path of the file to be converted
+    """
+
+	doc = Rtf15Reader.read(open(path))
+
+	return PlaintextWriter.write(doc).getvalue()
+
+def convertDocxToText(path):
+    """
+    This function converts Rtf to text
+
+    path: path of the file to be converted
+    """
+
+	document = Document(path)
+
+	return "\n".join([para.text for para in document.paragraphs])
+'''
+
+if __name__ == '__main__':
+    f = convertPDFToText("ResumeHarsha_AIscientist.pdf")
+    print(f)
---
+++ resumeparser.py
@@ -0,0 +1,10 @@
+# !/Users/kbsriharsha/anaconda3/bin/python
+# coding: utf-8
+# author: Bharat Sri Harsha karpurapu
+
+#Importing libraries
+import sys
+import os
+import pandas as pd
+import numpy as np
+import json
