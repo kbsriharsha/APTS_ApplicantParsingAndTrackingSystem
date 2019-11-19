7caff8b86dc30b39ca7adf752b3f22d74e2ef452
Bharat Sri Harsha Karpurapu <kbsriharsha@Bharats-MBP.fios-router.home>
2019-11-18 23:48:32 -0500

    adding final texts

--- TextConverter.py
+++ TextConverter.py
@@ -11,6 +11,8 @@ input file (pdf, doc, etc) to text (.txt)
 # Importing libraries
 import sys
 import os
+import re
+import pyap
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
@@ -71,6 +73,65 @@ def convertDocxToText(path):
 	return "\n".join([para.text for para in document.paragraphs])
 '''
 
+def extract_email(text):
+    '''
+    Helper function to extract email id from text
+    :param text: plain text extracted from resume file
+    '''
+    email = re.findall(r"([^@|\s]+@[^@]+\.[^@|\s]+)", text)
+    if email:
+        try:
+            return email[0].split()[0].strip(';')
+        except IndexError:
+            return None
+
+def extract_mobile_number(text):
+    '''
+    Helper function to extract mobile number from text
+    :param text: plain text extracted from resume file
+    :return: string of extracted mobile numbers
+    '''
+    mob_num_regex = r'''\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}
+                    |\(\d{3}\)[-\.\s]*\d{3}[-\.\s]??\d{4}
+                    |\d{3}[-\.\s]??\d{4}
+                    |\(\d{3}[)-\.\s].\d{3}.\d{4}'''
+
+    phone = re.findall(re.compile(mob_num_regex), text)
+    if phone:
+        number = ''.join(phone[0])
+        return number
+
+def extract_address(text):
+    """
+    Extracts the address information
+
+    text: input text from where address to be extracted
+    """
+    return pyap.parse(re.sub(r',', "", f), country='US')[0]
+
+def extract_linkedin(text):
+    """
+    Extracts linkedin details
+
+    text: input text from where linkedin details have to be extracted
+    """
+    linkedin_reg = r'''https:\/\/www.linkedin.com\/in\/\w*'''
+    linkedin = re.findall(re.compile(linkedin_reg), text)
+    return linkedin[0]
+
+def extract_github(text):
+    """
+    Extracts linkedin details
+
+    text: input text from where linkedin details have to be extracted
+    """
+    github_req = r'''https:\/\/github.com\/\w*'''
+    github = re.findall(re.compile(github_req), text)
+    return github[0]
+
+
+
 if __name__ == '__main__':
-    f = convertPDFToText("ResumeHarsha_AIscientist.pdf")
-    print(f)
+    f = convertPDFToText("Resume_Harsha_Updated.pdf")
+    #print(get_total_experience('f'))
+    print(extract_github(f))
